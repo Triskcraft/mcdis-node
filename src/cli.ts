89c97f01fa3b0326852551a5e2505fc2fb5e5d62
Elí Maciel <tecno_eli@hotmail.com>
2026-06-08 21:44:27 +0000

    fix config token

--- src/cli.ts
+++ src/cli.ts
@@ -39,11 +39,11 @@ try {
 }
 
 const config = await loadConfig(configPath)
-const token = process.env.DISCORD_TOKEN ?? config.discord.token
+const token = process.env.DISCORD_TOKEN ?? config.discord.tokenEnv
 
 if (!token) {
 	throw new Error(
-		`Missing Discord token. Set DISCORD_TOKEN in the environment, .env file, or discord.token in the config.`,
+		`Missing Discord token. Set DISCORD_TOKEN in the environment, .env file, or discord.tokenEnv in the config.`,
 	)
 }
 
